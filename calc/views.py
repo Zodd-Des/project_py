039246f5efb87d963702bc9a094c116f97536dbd
Dess <desmondotachi1@gmail.com>
2020-05-06 14:48:00 +0300

    dynamic page

--- calc/views.py
+++ calc/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello my peoples");
+    return render(request, "home.html",{"name":"My Peoples"})
